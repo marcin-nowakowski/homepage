0ba9d9b12c9a3ab6f1ac4ff989deb02b72a7f04d
Marcin Nowakowski <marcin.nowakowski08@gmail.com>
2020-05-10 18:01:52 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('Witam na stronie testowej!');
+
+const test = 'Napis Testowy';
+const testNumber = 100;
+
+console.log('Witam Cię na stronie testowej');
+console.log(test);
+console.log(testNumber);
+
+console.log(`Jestes na stronie testowej, a to jest ${test} oraz liczba testowa ${testNumber}`);
